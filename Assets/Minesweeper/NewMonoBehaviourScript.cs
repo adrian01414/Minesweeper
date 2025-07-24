b44549a5c2fff0f61aaf30bd5abdb317f819cc18
Adrian <60646083+adrian01414@users.noreply.github.com>
2025-07-24 12:42:38 +0700

    init project

---
+++ Assets/Minesweeper/NewMonoBehaviourScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private const int qwe = 10;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int a;
+
+        a = qwe;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
